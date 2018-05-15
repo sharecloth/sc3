1e7e72adfbb4ee0c52c0018aa4a46194282aa4a4
petun <petun911@gmail.com>
2018-05-15 14:09:42 +0300

    WEB-2503 add php support

---
+++ _head.php
@@ -0,0 +1,34 @@
+<head>
+    <!-- Title -->
+    <title>ShareCloth – retail software for apparel on-demand manufacturing</title>
+
+    <!-- Required Meta Tags Always Come First -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon_sc2.png">
+
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans%3A400%2C300%2C500%2C600%2C700">
+
+    <!-- CSS Global Compulsory -->
+    <link rel="stylesheet" href="assets/vendor/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/vendor/icon-hs/style.css">
+    <link rel="stylesheet" href="assets/vendor/icon-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/vendor/icon-line-pro/style.css">
+    <link rel="stylesheet" href="assets/vendor/hs-megamenu/src/hs.megamenu.css">
+    <link rel="stylesheet" href="assets/vendor/animate.css">
+    <link rel="stylesheet" href="assets/vendor/slick-carousel/slick/slick.css">
+    <link rel="stylesheet" href="assets/vendor/hamburgers/hamburgers.min.css">
+
+
+    <!-- CSS Unify -->
+    <link rel="stylesheet" href="assets/css/unify-core.css">
+    <link rel="stylesheet" href="assets/css/unify-components.css">
+    <link rel="stylesheet" href="assets/css/unify-globals.css">
+
+    <!-- CSS Customization -->
+    <link rel="stylesheet" href="assets/css/custom.css">
+</head>
---
+++ contacts.php
@@ -0,0 +1,207 @@
+<?php include "_header.php";?>
+
+    <!-- Promo Block -->
+    <section class="u-bg-overlay g-bg-size-cover g-bg-pos-center g-bg-black-opacity-0_3--after" style="background-image: url(../../assets/img-temp/1920x800/img4.jpg);">
+      </div>
+      <div class="container g-color-white text-center g-pos-rel g-z-index-1 g-py-150">
+        <h1 class="h2 g-font-weight-300">It is good to meet you</h1>
+        <h2 class="display-2 g-font-weight-600 text-uppercase g-letter-spacing-1">Contact us</h2>
+      </div>
+    </section>
+    <!-- End Promo Block -->
+
+	<section class="clearfix g-brd-bottom g-brd-gray-light-v4">
+		<!-- Icons Block -->
+		<div class="row no-gutters">
+		  <div class="col-md-6 col-lg-3 g-brd-right--md g-brd-gray-light-v4">
+		    <!-- Icon Blocks -->
+		    <div class="text-center g-py-20">
+		      <span class="u-icon-v1 u-icon-size--xl g-color-black g-mb-10">
+		          <i class="icon-real-estate-027 u-line-icon-pro"></i>
+		        </span>
+		      <h4 class="h5 g-font-weight-600 g-mb-5">Address</h4>
+		      <span class="d-block">61 Oxford str., London, 3DG</span>
+		    </div>
+		    <!-- End Icon Blocks -->
+		  </div>
+
+		  <div class="col-md-6 col-lg-3 g-brd-right--md g-brd-gray-light-v4">
+		    <!-- Icon Blocks -->
+		    <div class="text-center g-py-20">
+		      <span class="u-icon-v1 u-icon-size--xl g-color-black g-mb-10">
+		          <i class="icon-communication-062 u-line-icon-pro"></i>
+		        </span>
+		      <h4 class="h5 g-font-weight-600 g-mb-5">Phone Number</h4>
+		      <span class="d-block">1-800-643-4500</span>
+		    </div>
+		    <!-- End Icon Blocks -->
+		  </div>
+
+		  <div class="col-md-6 col-lg-3 g-brd-right--md g-brd-gray-light-v4">
+		    <!-- Icon Blocks -->
+		    <div class="text-center g-py-20">
+		      <span class="u-icon-v1 u-icon-size--xl g-color-black g-mb-10">
+		          <i class="icon-electronics-005 u-line-icon-pro"></i>
+		        </span>
+		      <h4 class="h5 g-font-weight-600 g-mb-5">Email</h4>
+		      <span class="d-block">mail@htmlstream.com</span>
+		    </div>
+		    <!-- End Icon Blocks -->
+		  </div>
+
+		  <div class="col-md-6 col-lg-3">
+		    <!-- Icon Blocks -->
+		    <div class="text-center g-py-20">
+		      <span class="u-icon-v1 u-icon-size--xl g-color-black g-mb-10">
+		          <i class="icon-finance-027 u-line-icon-pro"></i>
+		        </span>
+		      <h4 class="h5 g-font-weight-600 g-mb-5">Toll Free</h4>
+		      <span class="d-block">@unify</span>
+		    </div>
+		    <!-- End Icon Blocks -->
+		  </div>
+		</div>
+		<!-- End Icons Block -->
+	</section>
+
+    <!-- Contacts -->
+    <section class="container g-py-70">
+      <div class="row">
+		<div class="col-lg-6 g-mb-60">
+			<h2 class="h3">Contact us</h2>
+			  <p class="g-color-gray-dark-v3 g-font-size-16">We are a creative studio focusing on culture, luxury, editorial &amp; art. Somewhere between sophistication and simplicity.</p>
+
+			  <hr class="g-my-40">
+
+			  <!-- Contact Form -->
+			  <form>
+			    <div class="g-mb-20">
+			      <label class="g-color-gray-dark-v2 g-font-weight-600 g-font-size-13">Your name: <span class="g-color-red">*</span>
+			      </label>
+			      <input class="form-control g-color-gray-dark-v5 g-bg-white g-bg-white--focus g-brd-primary--focus g-brd-gray-light-v4 rounded-3 g-py-13 g-px-15" type="text">
+			    </div>
+
+			    <div class="g-mb-20">
+			      <label class="g-color-gray-dark-v2 g-font-weight-600 g-font-size-13">Your email: <span class="g-color-red">*</span>
+			      </label>
+			      <input class="form-control g-color-gray-dark-v5 g-bg-white g-bg-white--focus g-brd-primary--focus g-brd-gray-light-v4 rounded-3 g-py-13 g-px-15" type="email">
+			    </div>
+
+			    <div class="g-mb-40">
+			      <label class="g-color-gray-dark-v2 g-font-weight-600 g-font-size-13">Your message: <span class="g-color-red">*</span>
+			      </label>
+			      <textarea class="form-control g-color-gray-dark-v5 g-bg-white g-bg-white--focus g-brd-primary--focus g-brd-gray-light-v4 g-resize-none rounded-3 g-py-13 g-px-15" rows="7"></textarea>
+			    </div>
+
+			    <button class="btn btn-lg u-btn-primary g-font-weight-600 g-font-size-default rounded-3 text-uppercase g-py-15 g-px-30" type="submit" role="button">Send Enquiry</button>
+			  </form>
+			  <!-- End Contact Form -->
+		</div>
+
+        <div class="col-lg-5 offset-lg-1 g-bg-gray-dark-v1">
+          <div class="g-pa-50 g-pa-70--md">
+            <h2 class="h2 g-color-white">Our office</h2>
+
+            <!-- Contact Details -->
+            <ul class="list-unstyled g-color-white-opacity-0_7 g-font-weight-300 g-font-size-13 g-line-height-1_8 mb-0">
+              <li class="g-my-30">
+                <div class="media">
+                  <i class="d-flex g-color-primary g-font-size-20 mt-1 mr-4 icon-hotel-restaurant-235 u-line-icon-pro"></i>
+                  <div class="media-body">
+                    New York,
+                    <br>Unit 25 Suite 3, 925 Prospect PI
+                  </div>
+                </div>
+              </li>
+              <li class="g-my-30">
+                <div class="media">
+                  <i class="d-flex g-color-primary g-font-size-20 mt-1 mr-4 icon-communication-062 u-line-icon-pro"></i>
+                  <div class="media-body">
+                    info@gmail.com
+                  </div>
+                </div>
+              </li>
+              <li class="g-my-30">
+                <div class="media">
+                  <i class="d-flex g-color-primary g-font-size-20 mt-1 mr-4 icon-communication-033 u-line-icon-pro"></i>
+                  <div class="media-body">
+                    +32 333 444 555
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="media">
+                  <i class="d-flex g-color-primary g-font-size-20 mt-1 mr-4 icon-education-097 u-line-icon-pro"></i>
+                  <div class="media-body">
+                    <h2 class="g-color-white-opacity-0_9 g-font-size-default g-font-weight-600 text-uppercase mb-3">Office hours:</h2>
+                    <div class="mb-3">
+                      <span class="d-block g-color-white">Monday - Firday</span>
+                      09:00 AM - 06:00 PM
+                    </div>
+                    <div class="mb-3">
+                      <span class="d-block g-color-white">Saturday</span>
+                      12:00 AM - 03:00 PM
+                    </div>
+                    <span class="d-block g-color-white">Sunday &amp; Holidays</span>
+                    Closed
+                  </div>
+                </div>
+              </li>
+            </ul>
+            <!-- End Contact Details -->
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Contacts -->
+
+    <hr class="g-brd-gray-light-v4 my-0">
+
+    <!-- Clients -->
+    <div class="container g-py-70">
+      <div class="js-carousel" data-infinite="true" data-autoplay="true" data-slides-show="6" data-responsive='[{
+               "breakpoint": 768,
+               "settings": {
+                 "slidesToShow": 1
+               }
+             }]'>
+        <div class="js-slide u-block-hover">
+          <img class="mx-auto g-width-80 u-block-hover__main--grayscale g-opacity-0_3 g-opacity-1--hover g-cursor-pointer" src="../../assets/img-temp/200x100/img1.png" alt="Image Description">
+        </div>
+
+        <div class="js-slide u-block-hover">
+          <img class="mx-auto g-width-80 u-block-hover__main--grayscale g-opacity-0_3 g-opacity-1--hover g-cursor-pointer" src="../../assets/img-temp/200x100/img2.png" alt="Image Description">
+        </div>
+
+        <div class="js-slide u-block-hover">
+          <img class="mx-auto g-width-80 u-block-hover__main--grayscale g-opacity-0_3 g-opacity-1--hover g-cursor-pointer" src="../../assets/img-temp/200x100/img3.png" alt="Image Description">
+        </div>
+
+        <div class="js-slide u-block-hover">
+          <img class="mx-auto g-width-80 u-block-hover__main--grayscale g-opacity-0_3 g-opacity-1--hover g-cursor-pointer" src="../../assets/img-temp/200x100/img4.png" alt="Image Description">
+        </div>
+
+        <div class="js-slide u-block-hover">
+          <img class="mx-auto g-width-80 u-block-hover__main--grayscale g-opacity-0_3 g-opacity-1--hover g-cursor-pointer" src="../../assets/img-temp/200x100/img5.png" alt="Image Description">
+        </div>
+
+        <div class="js-slide u-block-hover">
+          <img class="mx-auto g-width-80 u-block-hover__main--grayscale g-opacity-0_3 g-opacity-1--hover g-cursor-pointer" src="../../assets/img-temp/200x100/img6.png" alt="Image Description">
+        </div>
+
+        <div class="js-slide u-block-hover">
+          <img class="mx-auto g-width-80 u-block-hover__main--grayscale g-opacity-0_3 g-opacity-1--hover g-cursor-pointer" src="../../assets/img-temp/200x100/img7.png" alt="Image Description">
+        </div>
+
+        <div class="js-slide u-block-hover">
+          <img class="mx-auto g-width-80 u-block-hover__main--grayscale g-opacity-0_3 g-opacity-1--hover g-cursor-pointer" src="../../assets/img-temp/200x100/img8.png" alt="Image Description">
+        </div>
+
+        <div class="js-slide u-block-hover">
+          <img class="mx-auto g-width-80 u-block-hover__main--grayscale g-opacity-0_3 g-opacity-1--hover g-cursor-pointer" src="../../assets/img-temp/200x100/img9.png" alt="Image Description">
+        </div>
+      </div>
+    </div>
+    <!-- End Clients -->
+
+<?php include "_footer.php";
