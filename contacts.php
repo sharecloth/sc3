171056a6de0141c48a3fc3c324fdabe7e47ed25d
sergey2k <sergey2k@gmail.com>
2018-05-17 17:11:22 +0300

    Contacts updates

--- contacts.php
+++ contacts.php
@@ -16,34 +16,33 @@
 		    <!-- Icon Blocks -->
 		    <div class="text-center g-py-20">
 		      <span class="u-icon-v1 u-icon-size--xl g-color-black g-mb-10">
-		          <i class="icon-real-estate-027 u-line-icon-pro"></i>
+		          <i class="icon-electronics-005 u-line-icon-pro"></i>
 		        </span>
-		      <h4 class="h5 g-font-weight-600 g-mb-5">Address</h4>
-		      <span class="d-block">61 Oxford str., London, 3DG</span>
+		      <h4 class="h5 g-font-weight-600 g-mb-5">Online call</h4>
+		      <span class="d-block">
+				<a class="zingayaButton zingaya0398fcfa7a2f41c7b9935d9b14296873" id="zingayaButton0398fcfa7a2f41c7b9935d9b14296873" href="https://zingaya.com/widget/0398fcfa7a2f41c7b9935d9b14296873" onclick="window.open(this.href+'?referrer='+escape(window.location.href)+'', '_blank', 'width=236,height=220,resizable=no,toolbar=no,menubar=no,location=no,status=no'); return false;">Call us</a></span>
 		    </div>
 		    <!-- End Icon Blocks -->
 		  </div>
-
+		  <div class="col-md-6 col-lg-3 g-brd-right--md g-brd-gray-light-v4">
+		    <!-- Icon Blocks -->
+		    <div class="text-center g-py-20">
+		      <span class="u-icon-v1 u-icon-size--xl g-color-black g-mb-10">
+		          <i class="icon-real-estate-027 u-line-icon-pro"></i>
+		        </span>
+		      <h4 class="h5 g-font-weight-600 g-mb-5">Chat now</h4>
+		      <span class="d-block"><a id="support_chat" onclick="Chatra('show'); Chatra('openChat', true)" href="#">Open chat</a>
+		    </div>
+		    <!-- End Icon Blocks -->
+		  </div>
 		  <div class="col-md-6 col-lg-3 g-brd-right--md g-brd-gray-light-v4">
 		    <!-- Icon Blocks -->
 		    <div class="text-center g-py-20">
 		      <span class="u-icon-v1 u-icon-size--xl g-color-black g-mb-10">
 		          <i class="icon-communication-062 u-line-icon-pro"></i>
 		        </span>
-		      <h4 class="h5 g-font-weight-600 g-mb-5">Phone Number</h4>
-		      <span class="d-block">1-800-643-4500</span>
-		    </div>
-		    <!-- End Icon Blocks -->
-		  </div>
-
-		  <div class="col-md-6 col-lg-3 g-brd-right--md g-brd-gray-light-v4">
-		    <!-- Icon Blocks -->
-		    <div class="text-center g-py-20">
-		      <span class="u-icon-v1 u-icon-size--xl g-color-black g-mb-10">
-		          <i class="icon-electronics-005 u-line-icon-pro"></i>
-		        </span>
 		      <h4 class="h5 g-font-weight-600 g-mb-5">Email</h4>
-		      <span class="d-block">mail@htmlstream.com</span>
+		      <span class="d-block">hi@sharecloth.com</span>
 		    </div>
 		    <!-- End Icon Blocks -->
 		  </div>
@@ -52,10 +51,11 @@
 		    <!-- Icon Blocks -->
 		    <div class="text-center g-py-20">
 		      <span class="u-icon-v1 u-icon-size--xl g-color-black g-mb-10">
-		          <i class="icon-finance-027 u-line-icon-pro"></i>
+		          <i class="icon-real-estate-027 u-line-icon-pro"></i>
 		        </span>
-		      <h4 class="h5 g-font-weight-600 g-mb-5">Toll Free</h4>
-		      <span class="d-block">@unify</span>
+		      <h4 class="h5 g-font-weight-600 g-mb-5">Mail address</h4>
+		      <span class="d-block">ShareCloth, Inc.
+			Suit 210-A, 300 Delaware Ave., Willmington, DE, United States, 90801</span>
 		    </div>
 		    <!-- End Icon Blocks -->
 		  </div>
@@ -68,7 +68,7 @@
       <div class="row">
 		<div class="col-lg-6 g-mb-60">
 			<h2 class="h3">Contact us</h2>
-			  <p class="g-color-gray-dark-v3 g-font-size-16">We are a creative studio focusing on culture, luxury, editorial &amp; art. Somewhere between sophistication and simplicity.</p>
+			  <p class="g-color-gray-dark-v3 g-font-size-16">We appreciate your interest to ShareCloth technologies and products. We'll get back to you as soon as possible</p>
 
 			  <hr class="g-my-40">
 
@@ -78,14 +78,13 @@
                 <div class="form-group"><label for="type">Request type</label>
                     <select id="type" name="type"
                             class="form-control g-color-gray-dark-v5 g-bg-white g-bg-white--focus g-brd-primary--focus g-brd-gray-light-v4 rounded-3 g-py-13 g-px-15">
-
+                        <option value="On-demand Product" selected="1">On-demand Product</option>	
                         <option value="Products - 3D retail inquiry">Products - "3D retail" inquiry</option>
                         <option value="Products - Product development inquiry">Products - "Product development"
                             inquiry
                         </option>
                         <option value="Services request">Services request</option>
-                        <option value="ShareCloth Creators">ShareCloth Creators</option>
-                        <option selected="1" value="Feedback">Feedback</option>
+                        <option value="Feedback">Feedback</option>
                         <option value="Support">Support</option>
                     </select>
                 </div>
