b09e55096dd46de34fa0c3eb987c5194d1e24979
sergey2k <sergey2k@gmail.com>
2018-06-01 19:38:17 -0400

    privacy policy page added

---
+++ pages/privacy.php
@@ -0,0 +1,89 @@
+<?php include "../_header.php";?>
+	<section class="g-bg-gray-light-v5 g-py-50">
+		<div class="container">
+			<div class="d-sm-flex text-center">
+				<div class="align-self-center">
+					<h2 class="h3 g-font-weight-300 w-100 g-mb-10 g-mb-0--md">Privacy policy</h2>
+				</div>
+				<div class="align-self-center ml-auto">
+					<ul class="u-list-inline">
+						<li class="list-inline-item g-color-primary">
+							<span>Privacy policy</span>
+							<i class="g-color-gray-light-v2 g-ml-5">/</i>
+						</li>
+						<li class="list-inline-item g-mr-5">
+							<a class="u-link-v5 g-color-main g-color-primary--hover" href="#!">Terms and Conditions</a>
+							<i class="g-color-gray-light-v2 g-ml-5">/</i>
+						</li>
+						<li class="list-inline-item g-mr-5">
+							<a class="u-link-v5 g-color-main g-color-primary--hover" href="#!">Pages</a>
+							<i class="g-color-gray-light-v2 g-ml-5">/</i>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="container g-py-150">
+	      <div class="row">
+	        <div class="col-lg-6">
+	          <div class="d-inline-block g-width-80 g-height-4 g-bg-black mb-3"></div>
+	          <h2 class="g-color-black g-font-weight-700 g-font-size-50 g-line-height-1 mb-4">Terms &amp; Conditions</h2>
+	          <p class="mb-0">This is where we sit down, grab a cup of coffee and dial in the details. Understanding the task at hand and ironing out the wrinkles is key. Now that we've aligned the details, it's time to get things mapped out and organized. This part is really
+	            crucial in keeping the project in line to completion.</p>
+	        </div>
+	      </div>
+	    </section>
+	
+		<section class="g-bg-gray-light-v5">
+		      <div class="container g-py-100">
+		        <div class="row">
+					<div class="col-md-8 offset-md-4">
+		            <!-- License -->
+		            <div id="license" class="u-shadow-v19 g-bg-white rounded g-pa-40 g-mb-50">
+	              		<h2 class="h3 g-color-black mb-3">License</h2>
+		              <div class="d-inline-block g-width-50 g-height-2 g-bg-black mb-3"></div>
+		              <p>This is where we sit down, grab a cup of coffee and dial in the details. Understanding the task at hand and ironing out the wrinkles is key. Now that we've aligned the details, it's time to get things mapped out and organized. This part
+		                is really crucial in keeping the project in line to completion.</p>
+		              <p>Whether through commerce or just an experience to tell your brand's story, the time has come to start using development languages that fit your projects needs. Now that your brand is all dressed up and ready to party, it's time to release
+		                it to the world. By the way, let's celebrate already. We get it, you're busy and it's important that someone keeps up with marketing and driving people to your brand. We've got you covered.</p>
+		            </div>
+		            <!-- End License -->
+
+		            <!-- Ownership -->
+		            <div id="ownership" class="u-shadow-v19 g-bg-white rounded g-pa-40 g-mb-50">
+		              <h3 class="g-color-black mb-3">Ownership</h3>
+		              <div class="d-inline-block g-width-50 g-height-2 g-bg-black mb-3"></div>
+		              <p>This is where we sit down, grab a cup of coffee and dial in the details. Understanding the task at hand and ironing out the wrinkles is key. Now that we've aligned the details, it's time to get things mapped out and organized. This part
+		                is really crucial in keeping the project in line to completion.</p>
+		              <p>Whether through commerce or just an experience to tell your brand's story, the time has come to start using development languages that fit your projects needs. Now that your brand is all dressed up and ready to party, it's time to release
+		                it to the world. By the way, let's celebrate already. We get it, you're busy and it's important that someone keeps up with marketing and driving people to your brand. We've got you covered.</p>
+		            </div>
+		            <!-- End Ownership -->
+
+		            <!-- Usage -->
+		            <div id="usage" class="u-shadow-v19 g-bg-white rounded g-pa-40 g-mb-50">
+		              <h3 class="g-color-black mb-3">Usage</h3>
+		              <div class="d-inline-block g-width-50 g-height-2 g-bg-black mb-3"></div>
+		              <p>This is where we sit down, grab a cup of coffee and dial in the details. Understanding the task at hand and ironing out the wrinkles is key. Now that we've aligned the details, it's time to get things mapped out and organized. This part
+		                is really crucial in keeping the project in line to completion.</p>
+		              <p>Whether through commerce or just an experience to tell your brand's story, the time has come to start using development languages that fit your projects needs. Now that your brand is all dressed up and ready to party, it's time to release
+		                it to the world. By the way, let's celebrate already. We get it, you're busy and it's important that someone keeps up with marketing and driving people to your brand. We've got you covered.</p>
+		            </div>
+		            <!-- End Usage -->
+
+		            <!-- Refunds -->
+		            <div id="refunds" class="u-shadow-v19 g-bg-white rounded g-pa-40">
+		              <h3 class="g-color-black mb-3">Refunds</h3>
+		              <div class="d-inline-block g-width-50 g-height-2 g-bg-black mb-3"></div>
+		              <p>This is where we sit down, grab a cup of coffee and dial in the details. Understanding the task at hand and ironing out the wrinkles is key. Now that we've aligned the details, it's time to get things mapped out and organized. This part
+		                is really crucial in keeping the project in line to completion.</p>
+		              <p>Whether through commerce or just an experience to tell your brand's story, the time has come to start using development languages that fit your projects needs. Now that your brand is all dressed up and ready to party, it's time to release
+		                it to the world. By the way, let's celebrate already. We get it, you're busy and it's important that someone keeps up with marketing and driving people to your brand. We've got you covered.</p>
+		            </div>
+		            <!-- End Refunds -->
+		          </div>
+		        </div>
+		      </div>
+	    </section>
+<?php include "../_footer.php";
