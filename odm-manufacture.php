45503b1eeb658546297d61908e353815123a46da
sergey2k <sergey2k@gmail.com>
2018-06-07 15:13:10 -0400

    ODM Manufacturers

---
+++ odm-manufacture.php
@@ -0,0 +1,135 @@
+<?php include "_header.php"; ?>
+<section class="g-bg-gray-light-v5">
+    <div class="container g-py-100">
+      <div class="row justify-content-center g-mb-60">
+        <div class="col-lg-8">
+          <!-- Heading -->
+          <div class="text-center">
+            <span class="d-block g-color-primary g-font-size-17 text-uppercase mb-5">We empower contractors</span>
+            <h2 class="h1 g-color-black g-font-weight-600 mb-5">To produce apparel on-demand in more efficient and cost-effective way</h2>
+            <p class="lead mb-5">ShareCloth is a solution that directly bridges apparel retail and manufactures to enable on-demand business model by digitizing and automating the supply chain</p>
+            <!--<a class="btn btn-xl u-btn-outline-black g-font-weight-600 g-font-size-default g-px-35" href="#!">Explore more</a>-->
+          </div>
+          <!-- End Heading -->
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-6 col-lg-3 g-mb-30">
+          <!-- Article -->
+          <article class="u-shadow-v21 u-shadow-v21--hover g-bg-white g-pos-rel g-rounded-5">
+            <img class="img-fluid w-100 g-rounded-top-5" src="../../assets/img-temp/350x230/img1.jpg" alt="Image Description">
+            <div class="g-pa-30">
+              <h3 class="h6 g-color-black g-font-weight-600 text-capitalize mb-3">
+                  <a class="g-color-black g-color-primary--hover g-text-underline--none--hover" href="#!">Receive</a>
+                </h3>
+              <p class="g-color-gray-dark-v4">Collect or accept style data (PDM) in the digital format</p>
+            </div>
+          </article>
+          <!-- End Article -->
+        </div>
+
+        <div class="col-sm-6 col-lg-3 g-mb-30">
+          <!-- Article -->
+          <article class="u-shadow-v21 u-shadow-v21--hover g-bg-white g-pos-rel g-rounded-5">
+            <img class="img-fluid w-100 g-rounded-top-5" src="../../assets/img-temp/350x230/img2.jpg" alt="Image Description">
+            <div class="g-pa-30">
+              <h3 class="h6 g-color-black g-font-weight-600 text-capitalize mb-3">
+                  <a class="g-color-black g-color-primary--hover g-text-underline--none--hover" href="#!">Accept</a>
+                </h3>
+              <p class="g-color-gray-dark-v4">Estimate each style in your proven economy</p>
+            </div>
+          </article>
+          <!-- End Article -->
+        </div>
+
+        <div class="col-sm-6 col-lg-3 g-mb-30">
+          <!-- Article -->
+          <article class="u-shadow-v21 u-shadow-v21--hover g-bg-white g-pos-rel g-rounded-5">
+            <img class="img-fluid w-100 g-rounded-top-5" src="../../assets/img-temp/350x230/img3.jpg" alt="Image Description">
+            <div class="g-pa-30">
+              <h3 class="h6 g-color-black g-font-weight-600 text-capitalize mb-3">
+                  <a class="g-color-black g-color-primary--hover g-text-underline--none--hover" href="#!">Produce</a>
+                </h3>
+              <p class="g-color-gray-dark-v4">Once approved start producing orders on-demand</p>
+            </div>
+          </article>
+          <!-- End Article -->
+        </div>
+
+        <div class="col-sm-6 col-lg-3 g-mb-30">
+          <!-- Article -->
+          <article class="u-shadow-v21 u-shadow-v21--hover g-bg-white g-pos-rel g-rounded-5">
+            <img class="img-fluid w-100 g-rounded-top-5" src="../../assets/img-temp/350x230/img4.jpg" alt="Image Description">
+            <div class="g-pa-30">
+              <h3 class="h6 g-color-black g-font-weight-600 text-capitalize mb-3">
+                  <a class="g-color-black g-color-primary--hover g-text-underline--none--hover" href="#!">Ship</a>
+                </h3>
+              <p class="g-color-gray-dark-v4">By your own or with a help of delivery service</p>
+            </div>
+          </article>
+          <!-- End Article -->
+        </div>
+      </div>
+    </div>
+</section>
+
+<!-- Icon Blocks -->
+<section>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 g-py-100 g-px-40--sm">
+        <!-- Icon Blocks -->
+        <div class="text-center">
+          <span class="u-icon-v2 g-color-white g-bg-primary rounded-circle mb-4">
+            <i class="icon-education-087 u-line-icon-pro"></i>
+          </span>
+          <h3 class="h5 g-color-black text-uppercase mb-3">Digital flow</h3>
+          <p class="g-color-gray-dark-v4">Reduce paperback work, speed up and secure your communication with the clients</p>
+        </div>
+        <!-- End Icon Blocks -->
+      </div>
+
+      <div class="col-lg-4 g-brd-left--lg g-brd-gray-light-v4 g-py-100 g-px-40--sm">
+        <!-- Icon Blocks -->
+        <div class="text-center">
+          <span class="u-icon-v2 g-color-white g-bg-primary rounded-circle mb-4">
+            <i class="icon-education-035 u-line-icon-pro"></i>
+          </span>
+          <h3 class="h5 g-color-black text-uppercase mb-3">Automated payments</h3>
+          <p class="g-color-gray-dark-v4">Receive payment per piece delivered, control your financials with the dashboard</p>
+        </div>
+        <!-- End Icon Blocks -->
+      </div>
+
+      <div class="col-lg-4 g-brd-left--lg g-brd-gray-light-v4 g-py-100 g-px-40--sm">
+        <!-- Icon Blocks -->
+        <div class="text-center">
+          <span class="u-icon-v2 g-color-white g-bg-primary rounded-circle mb-4">
+            <i class="icon-education-141 u-line-icon-pro"></i>
+          </span>
+          <h3 class="h5 g-color-black text-uppercase mb-3">Easy integration</h3>
+          <p class="g-color-gray-dark-v4">Explore how easy to integrate the solution to your existing business process</p>
+        </div>
+        <!-- End Icon Blocks -->
+      </div>
+    </div>
+  </div>
+</section>
+<!-- End Icon Blocks -->
+
+<!-- Hero Info -->
+<section class="g-bg-cover g-bg-size-cover g-bg-black-opacity-0_4--after g-py-120" style="background-image: url(../../assets/img-temp/1920x800/img2.jpg);">
+  <div class="container text-center g-color-white g-pos-rel g-z-index-1">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <h2 class="g-font-weight-300 g-font-size-45 g-mb-20">Join us <span class="g-color-primary g-font-weight-600">today</span></h2>
+        <p class="lead g-color-white-opacity-0_8 g-font-weight-300 g-mb-50">Get more information and a free of charge or commitments demo period by filling the form today!</p>
+        <a class="btn btn-xl u-btn-primary g-font-weight-600 g-font-size-default g-px-35" href="/contacts.php?from=odm">Contact us</a>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Hero Info -->
+
+<?php include "_footer.php";
