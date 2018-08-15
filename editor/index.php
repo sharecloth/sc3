48832cd6317fa9e96ad0a41bfdcb30793b88bf81
sergey2k <sergey2k@gmail.com>
2018-08-15 18:06:00 +0300

    /editor/ initial

---
+++ editor/index.php
@@ -0,0 +1,156 @@
+<?php include "../_header.php";?>
+<!-- Section Content -->
+<section id="home" class="g-theme-bg-gray-light-v1 g-pt-30 g-pb-50">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-lg-5 ml-lg-auto d-flex text-center text-md-left g-font-size-16 g-mb-50px">
+        <div class="align-self-center">
+          <h2 class="text-uppercase g-line-height-1_3 g-font-size-36 g-mb-20 g-mb-30--lg">Get your apparel  <br><strong> 3D rendered</strong></h2>
+          <p class="g-mb-20 g-mb-35--lg">ShareCloth introduces <strong>the Editor</strong>. Your assistant to export flat digital pattern into the virtual 3D sample and make precise fitting on the body mannequin</p>
+         <a href="#">
+         	<img src="../assets/sc/editor_download_windows.png" style="max-width:200px" alt="Download for Windows" />
+         </a>
+        </div>
+      </div>
+
+      <div class="col-md-6 col-lg-6 g-overflow-hidden">
+        <img class="img-fluid" src="../assets/sc/editor_main3.png" alt="Image description">
+      </div>
+    </div>
+  </div>
+</section>
+<!-- End Section Content -->
+
+<!-- Section Content -->
+<section id="benefits" class="g-py-90 g-bg-secondary">
+  <div class="container text-center g-max-width-750 g-font-size-16 g-mb-65">
+    <div class="text-uppercase g-line-height-1_3 g-mb-20">
+      <h2 class="g-line-height-1_3 g-font-size-36 mb-0">One app — <strong>many options</strong></h2>
+    </div>
+
+    <p class="mb-0">Using the Editor you may not just create precise digital garment samples for free but access to many more tools within ShareCloth' ecosystem</p>
+  </div>
+
+  <div class="container">
+    <!-- Row -->
+    <div class="row">
+      <div class="col-md-6 col-lg-4 g-mb-30">
+        <div class="text-center g-bg-gray-light-v5 g-py-40 g-px-30">
+          <span class="u-icon-v1 g-font-size-36 g-color-primary g-mb-30"><i class="fa fa-search"></i></span>
+          <h4 class="text-uppercase g-font-weight-700 g-font-size-11 g-color-black g-mb-20">SET-UP YOUR FABRIC BEHAVIOUR
+          </h4>
+          <p class="mb-0">Select basic fabric presets or create your own within a few minutes with easy internal wizard</p>
+        </div>
+      </div>
+
+      <div class="col-md-6 col-lg-4 g-mb-30">
+        <div class="text-center g-bg-gray-light-v5 g-py-40 g-px-30">
+          <span class="u-icon-v1 g-font-size-36 g-color-primary g-mb-30"><i class="fa fa-sliders"></i></span>
+          <h4 class="text-uppercase g-font-weight-700 g-font-size-11 g-color-black g-mb-20">
+		CREATE VIRTUAL MANNEQUIN FOR REQUIRED MEASUREMENTS
+          </h4>
+          <p class="mb-0">Apply more then 10 basic measurements to create precise body avatar within a seconds</p>
+        </div>
+      </div>
+
+      <div class="col-md-6 col-lg-4 g-mb-30 g-mb-0--lg">
+        <div class="text-center g-bg-gray-light-v5 g-py-40 g-px-30">
+          <span class="u-icon-v1 g-font-size-36 g-color-primary g-mb-30"><i class="fa fa-mobile"></i></span>
+          <h4 class="text-uppercase g-font-weight-700 g-font-size-11 g-color-black g-mb-20">
+		CREATE SAMPLES, BUILD LOOKS</h4>
+          <p class="mb-0">Use the Editor to create and fit the single sample or to build whole look of tops and bottoms</p>
+        </div>
+      </div>
+
+      <div class="col-md-6 col-lg-4 g-mb-30">
+        <div class="text-center g-bg-gray-light-v5 g-py-40 g-px-30">
+          <span class="u-icon-v1 g-font-size-36 g-color-primary g-mb-30"><i class="fa fa-cloud"></i></span>
+          <h4 class="text-uppercase g-font-weight-700 g-font-size-11 g-color-black g-mb-20">STORE YOUR 3D SAMPLES IN THE CLOUD
+          </h4>
+          <p class="mb-0">The Editor helps you to upload into the Cloud to have the access from any laptop, smartphone or table</p>
+        </div>
+      </div>
+
+      <div class="col-md-6 col-lg-4 g-mb-30 g-mb-0--md">
+        <div class="text-center g-bg-gray-light-v5 g-py-40 g-px-30">
+          <span class="u-icon-v1 g-font-size-36 g-color-primary g-mb-30"><i class="fa fa-user"></i></span>
+          <h4 class="text-uppercase g-font-weight-700 g-font-size-11 g-color-black g-mb-20">SHARE DESIGNS WITH YOUR CUSTOMERS</h4>
+          <p class="mb-0">Apply the power of the Cloud to your business. Enable online access and the fitting to your customers or partners</p>
+        </div>
+      </div>
+
+      <div class="col-md-6 col-lg-4">
+        <div class="text-center g-bg-gray-light-v5 g-py-40 g-px-30">
+          <span class="u-icon-v1 g-font-size-36 g-color-primary g-mb-30"><i class="fa fa-lock"></i></span>
+          <h4 class="text-uppercase g-font-weight-700 g-font-size-11 g-color-black g-mb-20">Manage privacy and access
+          </h4>
+          <p class="mb-0">Provide online access for your customers or teammates with the strict direct access to the needed models</p>
+        </div>
+      </div>
+    </div>
+    <!-- End Row -->
+  </div>
+</section>
+<!-- End Section Content -->
+
+<!-- Section Content -->
+<section id="howItWorks" class="g-bg-primary g-py-90">
+  <div class="container text-center g-max-width-750 g-font-size-16 g-mb-65">
+    <div class="text-uppercase g-line-height-1_3 g-mb-20">
+      <h2 class="g-line-height-1_3 g-font-size-36 g-color-white mb-0">Simple 3D render for <strong>your apparel</strong></h2>
+    </div>
+
+    <p class="g-color-white mb-0">ShareCloth Editor is available for Windows and Mac with the free non-commercial access to the ShareCloth Cloud</p>
+  </div>
+
+  <div class="container">
+    <!-- Carousel -->
+    <div class="js-carousel"
+             data-infinite="true"
+             data-arrows-classes="u-arrow-v1 g-pos-abs g-top-35x g-width-45 g-height-45 g-color-primary g-bg-white g-rounded-50x g-transition-0_2 g-transition--ease-in"
+             data-arrow-left-classes="fa fa-chevron-left g-left-0"
+             data-arrow-right-classes="fa fa-chevron-right g-right-0">
+      <div class="js-slide">
+        <div class="container text-center g-max-width-750">
+          <div class="g-mb-20">
+            <img class="d-inline-block img-responsive" src="../assets/sc/editor_avatargenerator_v2.png" alt="Image description">
+            <img class="d-inline-block img-responsive" src="../assets/sc/editor_physics_v2.png" alt="Image description">
+          </div>
+
+          <h4 class="h6 text-uppercase g-font-weight-700 g-color-white g-mb-15">User Manual</h4>
+          <p class="g-color-white-opacity-0_8 g-mb-30">Sed feugiat porttitor nunc, non dignissim ipsum vestibulum in. Donec in blandit dolor. Vivamus a fringilla lorem.</p>
+          <a class="btn btn-lg text-uppercase u-btn-white g-font-weight-700 g-font-size-12 g-rounded-10 g-px-25 g-py-12 mb-0" href="#!">Learn more</a>
+        </div>
+      </div>
+
+      <div class="js-slide">
+        <div class="container text-center g-max-width-750">
+          <div class="g-mb-20">
+            <img class="d-inline-block" src="assets/img-temp/mockups/mockup5.png" alt="Image description">
+            <img class="d-inline-block" src="assets/img-temp/mockups/mockup6.png" alt="Image description">
+          </div>
+
+          <h4 class="h6 text-uppercase g-font-weight-700 g-color-white g-mb-15">Made with love</h4>
+          <p class="g-color-white-opacity-0_8 g-mb-30">Sed feugiat porttitor nunc, non dignissim ipsum vestibulum in. Donec in blandit dolor. Vivamus a fringilla lorem, vel faucibus ante. Nunc ullamcorper, justo a iaculis elementum, enim orci viverra eros, fringilla porttitor lorem eros vel odio.</p>
+          <a class="btn btn-lg text-uppercase u-btn-white g-font-weight-700 g-font-size-12 g-rounded-10 g-px-25 g-py-12 mb-0" href="#!">Learn more</a>
+        </div>
+      </div>
+
+      <div class="js-slide">
+        <div class="container text-center g-max-width-750">
+          <div class="g-mb-20">
+            <img class="d-inline-block" src="assets/img-temp/mockups/mockup5.png" alt="Image description">
+            <img class="d-inline-block" src="assets/img-temp/mockups/mockup6.png" alt="Image description">
+          </div>
+
+          <h4 class="h6 text-uppercase g-font-weight-700 g-color-white g-mb-15">Usability and progresion</h4>
+          <p class="g-color-white-opacity-0_8 g-mb-30">Sed feugiat porttitor nunc, non dignissim ipsum vestibulum in. Donec in blandit dolor. Vivamus a fringilla lorem, vel faucibus ante. Nunc ullamcorper.</p>
+          <a class="btn btn-lg text-uppercase u-btn-white g-font-weight-700 g-font-size-12 g-rounded-10 g-px-25 g-py-12 mb-0" href="#!">Learn more</a>
+        </div>
+      </div>
+    </div>
+    <!-- End Carousel -->
+  </div>
+</section>
+<!-- End Section Content -->
+<?php include "../_footer.php";
